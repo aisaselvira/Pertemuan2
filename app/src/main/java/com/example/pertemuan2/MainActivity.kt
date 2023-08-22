c8b650ae45ec9000804a1e624e0eb9d69a17be44
Aisa Selvira <aisaselvira2020@gmail.com>
2023-08-22 09:57:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan2/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.pertemuan2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.pertemuan2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            test1.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Hello " + textName.text, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
